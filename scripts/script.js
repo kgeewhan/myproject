ac50ee4e6d7934c3c21a8edc9a0e3bb754a98ffc
Geewhan Kim <g1kim@ucsd.edu>
2021-06-17 23:41:08 -0700

    ginryu update

--- scripts/script.js
+++ scripts/script.js
@@ -5,4 +5,10 @@ img.addEventListener('click', () => {
     count++;
     var counter = document.getElementById("counter");
     counter.innerHTML = "Count = " + count; 
+
+    if (count > 50) {
+        img.src = "./pics/ginryu.jpg"
+    }
+    
 });
+
